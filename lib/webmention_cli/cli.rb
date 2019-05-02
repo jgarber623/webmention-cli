8b3c62966f950c4e9146ec2d371927bb4fc91e03
Jason Garber <jason@sixtwothree.org>
2019-05-01 22:19:40 -0400

    Update dependencies and implement changes

--- lib/webmention_cli/cli.rb
+++ lib/webmention_cli/cli.rb
@@ -13,7 +13,7 @@ module WebmentionCLI
       exit(endpoint_url ? true : false)
     rescue IndieWeb::Endpoints::ArgumentError,
            IndieWeb::Endpoints::InvalidURIError
-      raise Thor::MalformattedArgumentError, "ERROR: Expected '#{url}' to be an absolute URI (e.g. https://example.com)"
+      raise Thor::MalformattedArgumentError, "ERROR: Expected '#{url}' to be an absolute URL (e.g. https://example.com)"
     end
 
     # desc 'send <source> <target>', 'Send a webmention from <source> URL to <target> URL'
@@ -25,7 +25,7 @@ module WebmentionCLI
     desc 'verify <source> <target>', 'Verify <source> URL links to <target> URL'
     option :strict, type: :boolean, desc: 'Enable or disable strict URL matching', default: true
     def verify(source, target)
-      webmention_verified = Webmention::Verification::Client.new(source, target, strict: options[:strict]).verified?
+      webmention_verified = Webmention::Verification.client(source, target, strict: options[:strict]).verified?
 
       if webmention_verified
         puts "SUCCESS: #{source} links to #{target}"
@@ -35,7 +35,7 @@ module WebmentionCLI
 
       exit(webmention_verified)
     rescue Webmention::Verification::ArgumentError
-      raise Thor::MalformattedArgumentError, "ERROR: Expected '#{source}' and '#{target}' to be absolute URIs (e.g. https://example.com)"
+      raise Thor::MalformattedArgumentError, "ERROR: Expected '#{source}' and '#{target}' to be absolute URLs (e.g. https://example.com)"
     end
   end
 end
--- spec/lib/webmention_cli/cli_verify_spec.rb
+++ spec/lib/webmention_cli/cli_verify_spec.rb
@@ -3,7 +3,7 @@ describe WebmentionCLI::CLI, '.verify' do
 
   context 'when source is invalid' do
     it 'raises a MalformattedArgumentError' do
-      message = "ERROR: Expected '/foo' and '#{url}' to be absolute URIs (e.g. https://example.com)"
+      message = "ERROR: Expected '/foo' and '#{url}' to be absolute URLs (e.g. https://example.com)"
 
       expect { described_class.new.verify('/foo', url) }.to raise_error(Thor::MalformattedArgumentError, message)
     end
@@ -11,7 +11,7 @@ describe WebmentionCLI::CLI, '.verify' do
 
   context 'when target is invalid' do
     it 'raises a MalformattedArgumentError' do
-      message = "ERROR: Expected '#{url}' and '/foo' to be absolute URIs (e.g. https://example.com)"
+      message = "ERROR: Expected '#{url}' and '/foo' to be absolute URLs (e.g. https://example.com)"
 
       expect { described_class.new.verify(url, '/foo') }.to raise_error(Thor::MalformattedArgumentError, message)
     end
