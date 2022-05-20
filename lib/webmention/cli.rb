d38b2fe4400146b06c92f2f7bc971ab809475384
Jason Garber <jason@sixtwothree.org>
2022-05-19 22:12:04 -0400

    Remove Webmention::ClI.start method

--- lib/webmention/cli.rb
+++ lib/webmention/cli.rb
@@ -4,13 +4,4 @@ require 'thor'
 require 'webmention'
 
 require_relative 'cli/version'
-
 require_relative 'cli/runner'
-
-module Webmention
-  module CLI
-    def self.start
-      Runner.start
-    end
-  end
-end
